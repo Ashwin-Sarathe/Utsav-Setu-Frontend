2ae809bd31fe25c58c36dad71745849d5b16f67f
ashwinsarathe <ashwinsarathe04@gmail.com>
2026-04-01 10:19:58 +0530

    Small import error change

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate, Link } from 'react-router-dom';
 import { Mail, Lock, LogIn, User } from 'lucide-react';
